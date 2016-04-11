4830b2dc695118f6dfad06106184630ae63e9dda
Lucas <lucas.marot@etu-webschoolfactory.fr>
2016-04-11 11:42:11 +0200

    Text update
    parce que voilà

--- controllers/front/FaqController.php
+++ controllers/front/FaqController.php
@@ -5,12 +5,12 @@ class FaqControllerCore extends FrontController {
         parent::init();
 ​
         $questions = [
-            ["question"=> "First of all, I began with being troubled about what to question these fellows on.",
-                "answer"=> "Why it is those five demons are treated as one, and so on. If the five demons aren't together will any limitations in ability occur, and so on? If one dies or there is even one demon missing from their number, does it become impossible to demonstrate an effect in unison, and so on."],
-            ["question"=> "Though I can think of various things, well, I'll set that aside.",
-                "answer"=> "The Five Demon Squadron also, just likes Heat Demon-kun and the others, experienced 【Existence Evolution】 upon obtaining the title."],
-            ["question"=> "The race the five demons became was 【Karma Demon (Karma) - Subspecies.",
-                "answer"=> "To describe a 【Karma Demon】 briefly, it is not of the 【Demon Man (Lord)】 system but a higher species of the 【Big Demon (Ogre)】 system, it's such a thing."],
+            ["question"=> "Recherche rapide",
+                "answer"=> "Pour trouver l'article recherché, entrez un ou plusieurs mots clés dans la barre bleue Rechercher, en haut de toutes nos pages et cliquez sur le bouton Go. Vous pouvez également limiter votre recherche à une catégorie de produits grâce au menu déroulant."],
+            ["question"=> "Recherche détaillée",
+                "answer"=> "La recherche détaillée vous permet de définir vos recherches de façon plus précise avec des critères spécifiques à chaque catégorie. Pour accéder à la recherche détaillée, selon le type d'article : Choisissez l'une de nos boutiques depuis le menu déroulant à gauche de notre site, vous aurez alors la possibilité d'effectuer une recherche détaillée depuis la barre de navigation juste en dessous de la recherche rapide."],
+            ["question"=> "Nos boutiques",
+                "answer"=> "Vous pouvez accéder à la page de chacune de nos boutiques depuis le menu déroulant toutes nos boutiques à gauche de toutes nos pages. Vous y trouverez les meilleures ventes, les dernières sorties, les produits les plus recherchés ou les plus offerts, nos offres spéciales, les forums et bien d'autres informations spécifiques à chaque catégorie !"],
         ];
         $this->context->smarty->assign(array('questions'=> $questions));
     }
